bf5dab1e01b4ee0d0968a8d180a226c5c1660dc6
taiwocoker <taiwofcoker@gmail.com>
2021-01-15 21:34:41 +0100

    Add Loading component to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+
+const url = 'https://course-api.netlify.app/api/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  
 }
 
 export default App
