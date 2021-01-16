709218d1b3ddb0a906830f7bbed5b7e6d9a6632f
taiwocoker <taiwofcoker@gmail.com>
2021-01-16 22:44:59 +0100

    update setLoading

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     try {
       const response = await fetch(url)
       const tours = await response.json()
+      setLoading(false)
       setTours(tours)
     } catch (error) {
       setLoading(false)
